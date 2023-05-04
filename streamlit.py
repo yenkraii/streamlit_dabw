dbe228c1c725322074506d93c3eb6687dfed6247
yenkraii <102582449+yenkraii@users.noreply.github.com>
2023-05-04 13:48:30 +0800

    Update streamlit.py
    look nice nice

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Hello World!")
+
+st.text("this is using a text function!")
+
+st.header("A header function is being used to separate sections! ")
+
+df_fruit = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+st.dataframe(df_fruit)
+
+st.text("the dataframe() function is used to display the loaded dataframe!")
 
