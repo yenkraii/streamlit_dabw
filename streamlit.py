f9a0c6b42939f96a8306be66cafd71e92dca23b4
yenkraii <102582449+yenkraii@users.noreply.github.com>
2023-05-04 13:52:06 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,7 +9,16 @@ st.header("A header function is being used to separate sections! ")
 
 df_fruit = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+df_fruit = df_fruit.set_index("Fruit")
+
 st.dataframe(df_fruit)
 
 st.text("the dataframe() function is used to display the loaded dataframe!")
+
+# we create user interaction
+
+st.multiselect("This is a multiselect function creating a pick list",["need","pass in a","list"])
+
+st.multiselect("Pick fruit",list(df_fruit.index))
+
 
